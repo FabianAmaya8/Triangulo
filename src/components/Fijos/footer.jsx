3e612434205ca35d3d547c6d196c0f8fc47e53d9
Fabian_Amaya <123851947+FabianAmaya8@users.noreply.github.com>
2025-03-26 13:07:30 -0500

    cambiando footer para que solo aparezca yo

--- src/components/Fijos/footer.jsx
+++ src/components/Fijos/footer.jsx
@@ -1,23 +1,9 @@
 import "./footer.css";
 function PiedePagina(){
-    //crea un footer con el nombre de los 4 mienbros con link a su git
     return(
         <div className="footer-conteiner">
             <h2>Desarrollado por</h2>
-            <ul>
-                <li>
-                    <a href="https://github.com/FabianAmaya8">Fabian Amaya</a>
-                </li>
-                <li>
-                    <a href="https://github.com/REDMAN8883">Julian Beltran</a>
-                </li>
-                <li>
-                    <a href="https://github.com/DanielMMiranda17">Daniel Miranda</a>
-                </li>
-                <li>
-                    <a href="https://github.com/Zarpien06">Oscar Cruz</a>
-                </li>
-            </ul>
+            <a href="https://github.com/FabianAmaya8" target="_blank">Fabian Amaya</a>
         </div>
     )
 }
